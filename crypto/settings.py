9d2478c23441172cdfcc336cbc09beda1517420a
AbhishekSingh-77 <abhishek74518@gmail.com>
2025-07-14 11:26:37 +0530

    changes in settings.py

--- crypto/settings.py
+++ crypto/settings.py
@@ -20,13 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@1!px-33l5mtqp*g5d=sc3h_hddte$fd9xq5q(%j7o_fgd!fps'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-for-local')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['crypto-backend.onrender.com']
+# Dynamic
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
