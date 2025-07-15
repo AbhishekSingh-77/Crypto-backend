a90d73884771fa0a0bc527e91f8cb1157cc25273
AbhishekSingh-77 <abhishek74518@gmail.com>
2025-07-15 11:18:10 +0530

    change

--- crypto/settings.py
+++ crypto/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-for-local')
 
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'False'
 
 # Dynamic
 ALLOWED_HOSTS = ['crypto-backend-dj0q.onrender.com']
